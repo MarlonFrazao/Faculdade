ba6fb4adbd414488dc034437f976c5dee35cb41c
MarlonFrazao <frazao.marlon@gmail.com>
2018-10-02 23:04:43 -0300

    FacadeAplicacao e AplicacaoBean parcial

---
+++ PrjIntegradorIV/src/br/edu/unifacear/projetointegrador4/bean/AplicacaoBean.java
@@ -0,0 +1,56 @@
+package br.edu.unifacear.projetointegrador4.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import br.edu.unifacear.projetointegrador4.model.entity.Aplicacao;
+import br.edu.unifacear.projetointegrador4.model.facade.FacadeAplicacao;
+
+@ManagedBean(name="aplicacaoBean")
+@RequestScoped
+public class AplicacaoBean {
+	
+	private Aplicacao aplicacao;
+	private FacadeAplicacao facade;
+	
+	public AplicacaoBean() {
+		aplicacao = new Aplicacao();
+		facade = new FacadeAplicacao();
+	}
+
+	public Aplicacao getAplicacao() {
+		return aplicacao;
+	}
+
+	public void setAplicacao(Aplicacao aplicacao) {
+		this.aplicacao = aplicacao;
+	}
+
+	public String inserir() {
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		try {
+			facade.inserir(aplicacao);
+			return "sucesso";
+		} catch(Exception e) {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, 
+					e.getMessage(),""));
+			return "errow";
+		}
+	}
+	
+	public String atualizar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		
+		try {
+			facade.atualizar(aplicacao);
+			return "sucesso";
+		} catch(Exception e) {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, 
+					e.getMessage(),""));
+			return "errow";
+		}
+	}
+}
---
+++ PrjIntegradorIV/src/br/edu/unifacear/projetointegrador4/model/facade/FacadeAplicacao.java
@@ -0,0 +1,46 @@
+package br.edu.unifacear.projetointegrador4.model.facade;
+
+import java.util.List;
+
+import br.edu.unifacear.projetointegrador4.model.business.AplicacaoBusiness;
+import br.edu.unifacear.projetointegrador4.model.business.BusinessException;
+import br.edu.unifacear.projetointegrador4.model.entity.Aplicacao;
+
+public class FacadeAplicacao {
+	
+	public void inserir(Aplicacao a) throws BusinessException {
+		new AplicacaoBusiness().inserir(a);
+	}
+
+	public void atualizar(Aplicacao a) throws BusinessException {
+		new AplicacaoBusiness().atualizar(a);
+	}
+
+	public List<Aplicacao> listar() throws BusinessException {
+		return new AplicacaoBusiness().listar();
+	}
+	
+	public List<Aplicacao> listar(Boolean status) throws BusinessException {
+		return new AplicacaoBusiness().listar(status);
+	}
+
+	public Aplicacao obter(Long id) throws BusinessException {
+		return new AplicacaoBusiness().obter(id);
+	}
+
+	public List<Aplicacao> obter(String descricao) throws BusinessException {
+		return new AplicacaoBusiness().obter(descricao);
+	}
+	
+	public List<Aplicacao> obter(String descricao, Boolean status) throws BusinessException {
+		return new AplicacaoBusiness().obter(descricao, status);
+	}
+
+	public void excluir(Aplicacao a) {
+		new AplicacaoBusiness().excluir(a);
+	}
+	
+	public void reativar(Aplicacao a) {
+		new AplicacaoBusiness().reativar(a);
+	}
+}
