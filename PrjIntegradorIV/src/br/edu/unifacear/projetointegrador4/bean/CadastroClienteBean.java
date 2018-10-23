143c714135c12a5f632b1cdda47528e8eb107c1c
osvaldoNovinski <37417572+osvaldoNovinski@users.noreply.github.com>
2018-10-23 17:59:25 -0200

    login modificado

--- PrjIntegradorIV/src/br/edu/unifacear/projetointegrador4/bean/CadastroClienteBean.java
+++ PrjIntegradorIV/src/br/edu/unifacear/projetointegrador4/bean/CadastroClienteBean.java
@@ -7,6 +7,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.primefaces.PrimeFaces;
 
 import br.edu.unifacear.projetointegrador4.model.entity.Cliente;
 import br.edu.unifacear.projetointegrador4.model.entity.Telefone;
@@ -17,13 +20,13 @@ import br.edu.unifacear.projetointegrador4.model.facade.FacadeTelefone;
 @RequestScoped
 public class CadastroClienteBean {
 
-	private Cliente cliente =null;
-	private List<Telefone> telefone=null;
-	private Cliente login =null;
+	private Cliente cliente = null;
+	private List<Telefone> telefone = null;
+	private Cliente login = null;
 	private FacadeCliente facade = null;
-	private Long confirmaSenha =null ;
-	private Telefone telefone2=null;
-	private FacadeTelefone facTel=null;
+	private Long confirmaSenha = null;
+	private Telefone telefone2 = null;
+	private FacadeTelefone facTel = null;
 
 	public CadastroClienteBean() {
 		System.out.println("Entrou no bean");
@@ -34,35 +37,24 @@ public class CadastroClienteBean {
 		telefone = new ArrayList<Telefone>();
 		telefone2 = new Telefone();
 		login = new Cliente();
-		
 
 	}
-	
-	
-	
+
 	public Telefone getTelefone2() {
 		return telefone2;
 	}
-
-
 
 	public void setTelefone2(Telefone telefone2) {
 		this.telefone2 = telefone2;
 	}
 
-
-
 	public Long getConfirmaSenha() {
 		return confirmaSenha;
 	}
 
-
-
 	public void setConfirmaSenha(Long confirmaSenha) {
 		this.confirmaSenha = confirmaSenha;
 	}
-
-
 
 	public Cliente getCliente() {
 		return cliente;
@@ -70,7 +62,7 @@ public class CadastroClienteBean {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
-		System.out.println("entrou set cli: "+cliente.getCpf());
+		System.out.println("entrou set cli: " + cliente.getCpf());
 	}
 
 	public List<Telefone> getTelefone() {
@@ -80,32 +72,30 @@ public class CadastroClienteBean {
 	public void setTelefone(List<Telefone> telefone) {
 		this.telefone = telefone;
 	}
+
 	public Cliente getLogin() {
 		return login;
 	}
+
 	public void setLogin(Cliente login) {
 		this.login = login;
 	}
 
-
-
 	public String inserir() {
-			System.out.println("entra 1");
+		System.out.println("entra 1");
 		FacesContext context = FacesContext.getCurrentInstance();
 		try {
 			System.out.println("entra 2");
-				telefone2.setCpf(cliente.getCpf());
-				facTel.inserirTelefone(telefone2);
-				telefone.add(telefone2);
-				cliente.setTelefone(telefone);
-				
-				
-				facade.inserirCliente(cliente);
-				
-				
-				return "sucesso";
-			
-				
+			telefone2.setCpf(cliente.getCpf());
+			facTel.inserirTelefone(telefone2);
+			telefone.add(telefone2);
+			cliente.setTelefone(telefone);
+
+			facade.inserirCliente(cliente);
+
+			login();
+			return "sucesso";
+
 		} catch (Exception e) {
 			System.out.println("entra exception inserir ");
 			System.out.println("-----" + e.getMessage());
@@ -113,44 +103,55 @@ public class CadastroClienteBean {
 			return "cadastrousuario";
 		}
 	}
-	
+
 	public String confirmaSenha() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		try {
-			if(this.cliente.getSenha() == this.confirmaSenha) {
+			if (this.cliente.getSenha() == this.confirmaSenha) {
 				return "ok";
-			}else {
+			} else {
 				context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Senhas Não Conferem", null));
 				return "cadastrarcliente";
 			}
-		}
-			catch(Exception e) {
+		} catch (Exception e) {
 			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), ""));
 			return "cadastrarcliente";
 		}
 	}
-	public String login() {
-		FacesContext context = FacesContext.getCurrentInstance();	
+
+	public void login() {
+
 		System.out.println("valida senha login");
+		FacesMessage message = null;
+		boolean loggedIn = false;
+
 		try {
 			System.out.println("valida senha login try");
 			this.login = facade.obterCliente(cliente.getCpf(), cliente.getSenha()).get(0);
-			System.out.println("valida senha login obter nome: "+login.getNome()+ " cpf "+login.getCpf());
-			if(this.login.getNome() != null) {
+			System.out.println("valida senha login obter nome: " + login.getNome() + " cpf " + login.getCpf());
+			if (this.login.getNome() != null) {
+				loggedIn = true;
+				message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bem Vindo ", login.getNome().toString());
+
 				System.out.println("valida senha login retornou login");
-				return "login";
-			}else {
-				context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Senha ou login inválido", null));
+				// return "login";
+			} else {
+				loggedIn = false;
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login inválido", "CPF ou Senha incorretos");
+
 				System.out.println("valida senha login retornou falha");
-				return "falha";
-				
+				// return "falha";
+
 			}
-		}catch(Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, message);
+			PrimeFaces.current().ajax().addCallbackParam("loggedIn", loggedIn);
+		} catch (Exception e) {
+
 			System.out.println("valida senha login exception");
-			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), ""));
-			return "falha";
+
+			// return "falha";
 		}
-		
+
 	}
 
 }
