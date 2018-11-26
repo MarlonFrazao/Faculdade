dcfc74ee1c43db75720ba4aa824fcbedd605be25
osvaldoNovinski <37417572+osvaldoNovinski@users.noreply.github.com>
2018-11-26 16:33:00 -0200

    Atualizações

--- PrjIntegradorIV/src/br/edu/unifacear/projetointegrador4/model/entity/PedidoDeVenda_.java
+++ PrjIntegradorIV/src/br/edu/unifacear/projetointegrador4/model/entity/PedidoDeVenda_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2018-11-24T18:02:56.719-0200")
+@Generated(value="Dali", date="2018-11-26T14:03:02.846-0200")
 @StaticMetamodel(PedidoDeVenda.class)
 public class PedidoDeVenda_ {
 	public static volatile SingularAttribute<PedidoDeVenda, Long> id;
@@ -14,6 +14,6 @@ public class PedidoDeVenda_ {
 	public static volatile SingularAttribute<PedidoDeVenda, Date> data;
 	public static volatile SingularAttribute<PedidoDeVenda, StatusPV> statusPV;
 	public static volatile SingularAttribute<PedidoDeVenda, Boolean> status;
-	public static volatile ListAttribute<PedidoDeVenda, PecasDoPedido> pecaspdv;
 	public static volatile SingularAttribute<PedidoDeVenda, Double> totalPedido;
+	public static volatile ListAttribute<PedidoDeVenda, PecasDoPedido> pecaspdv;
 }
